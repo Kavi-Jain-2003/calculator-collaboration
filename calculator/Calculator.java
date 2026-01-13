04c1a5bf830ba165534224298ad49632cd0f24bc
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-01-13 15:30:52 +0530

    Adding multiplication method

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -2,12 +2,18 @@ package calculator;
 public class Calculator
 {
     public static void main(String[] args) {
-        int result=add(2,3);
-        System.out.println("addition:"+result);
+        int resultAdd=add(2,3);
+        int resultMul=mul(4,5);
+        System.out.println("addition:"+resultAdd);
+        System.out.println("multiplication:"+resultMul);
     }
     public static int add(int a, int b)
     {
         return a+b;
+    }
+    public static int mul(int a, int b)
+    {
+        return a*b;
     }
     
 }
