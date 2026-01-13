232133a393e6cc7b00ab14349847555dcce577aa
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-01-13 15:22:39 +0530

    Adding calculator folder with add method

---
+++ calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+public class Calculator
+{
+    public static void main(String[] args) {
+        int result=add(2,3);
+        System.out.println("addition:"+result);
+    }
+    public static int add(int a, int b)
+    {
+        return a+b;
+    }
+    
+}
